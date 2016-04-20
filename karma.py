623f6df59d7d0126637656ca2346b8975785c9bf
Vasiliy Kaygorodov <vkaygorodov@gmail.com>
2016-04-20 11:23:45 +0200

    I will read the docs first... Apparently that should be "to", not "target"

--- karma.py
+++ karma.py
@@ -22,7 +22,7 @@ class Karma(BotPlugin):
         for word in message.body.split():
             word = word[:64]
             if word[:-2] == frm:
-                    self.send(message.target, 
+                    self.send(message.to, 
                     "Public masturbation is not allowed, {}".format(frm), message)
                     continue
             if word.endswith('++'):
@@ -55,7 +55,7 @@ class Karma(BotPlugin):
             reply = "{} have just pimped various karma".format(frm)
         else:
             return True
-        self.send(message.target, reply, message)
+        self.send(message.to, reply, message)
         return True
 
     def _draw_unicorn(self, message, extra):
@@ -71,12 +71,14 @@ class Karma(BotPlugin):
                    :   .'             
                     ''`
 """.format(extra)
-        self.send(message.target, unicorn, message)
+        self.send(message.to, unicorn, message)
 
     # Passing split_args_with=None will cause arguments to be split on any kind
     # of whitespace, just like Python's split() does
     @botcmd(split_args_with=None)
     def srank(self, mess, args):
+        """Shows top 10 karma rankings"""
+
         self.log.info("Processing SRANK with args: {}".format(args))
         try:
             karma = self['karma']
@@ -88,8 +90,3 @@ class Karma(BotPlugin):
         for elem in sorted_karma[0:10]:
             rank += "{} {}\n".format(elem[0], elem[1])
         return rank
-
-    @botcmd
-    def _draw(self, mess, args):
-        self._draw_unicorn(mess)
-        return True
